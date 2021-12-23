56ae5534d77dcf02c5feeaa5b5f97dc42983fff7
sunmin <sunm309@naver.com>
2021-12-23 15:54:10 +0900

    📝 section1 문제 풀이 복습

--- algorithm/inflearn/section1/study.js
+++ algorithm/inflearn/section1/study.js
@@ -35,14 +35,14 @@
 {
   // 삼각형 판별하기 (a, b, c 크기 비교해서 큰 수 max로 초기화 -> (a+b+c) - max <= max ? "No" : "YES" )
   function solution(a, b, c) {
-    let answer = "YES",
+    let answer = 'YES',
       max;
     let sum = a + b + c;
 
     if (a > b) max = a;
     else max = b;
     if (c > max) max = c;
-    if (sum - max <= max) answer = "NO";
+    if (sum - max <= max) answer = 'NO';
 
     return answer;
   }
@@ -179,9 +179,9 @@ console.clear();
 {
   // A를 #으로
   function solution(s) {
-    let answer = "";
+    let answer = '';
     for (let x of s) {
-      if (x === "A") answer += "#";
+      if (x === 'A') answer += '#';
       else answer += x;
     }
 
@@ -189,16 +189,16 @@ console.clear();
   }
 
   // 정규식 이용
-  function solution2(s) {
-    let answer = s;
-    answer = answer.replace(/A/g, "#");
+  // function solution2(s) {
+  //   let answer = s;
+  //   answer = answer.replace(/A/g, '#');
 
-    return answer;
-  }
+  //   return answer;
+  // }
 
-  let str = "BANANA";
+  let str = 'BANANA';
   console.log(solution(str));
-  console.log(solution2(str));
+  // console.log(solution2(str));
 }
 console.clear();
 {
@@ -212,16 +212,16 @@ console.clear();
     return answer;
   }
 
-  let str = "COMPUTERPROGRAMMING";
-  console.log(solution(str, "R"));
+  let str = 'COMPUTERPROGRAMMING';
+  console.log(solution(str, 'R'));
 
-  function solution2(s, t) {
-    let answer = s.split(t).length;
-    return answer - 1;
-  }
+  // function solution2(s, t) {
+  //   let answer = s.split(t).length;
+  //   return answer - 1;
+  // }
 
-  let str2 = "COMPUTERPROGRAMMING";
-  console.log(solution2(str2, "R"));
+  // let str2 = 'COMPUTERPROGRAMMING';
+  // console.log(solution2(str2, 'R'));
 }
 
 {
@@ -240,25 +240,135 @@ console.clear();
     return answer;
   }
 
-  let str = "KoreaTimeGood";
+  let str = 'KoreaTimeGood';
   console.log(solution(str));
 }
 console.clear();
 {
   // 대문자로 통일
+  function solution(s) {
+    // let answer = '';
+
+    for (const x of s) {
+      // (1)
+      // if (x === x.toLowerCase()) answer += x.toUpperCase();
+      // else answer += x;
+      // (2)
+      // let num = x.charCodeAt();
+      // if (num >= 97 && num <= 122) answer += String.fromCharCode(num - 32);
+      // else answer += x;
+    }
+
+    // (3)
+    let answer = s.toUpperCase();
+
+    return answer;
+  }
+  let str = 'ItisTimeToStudy';
+  console.log(solution(str));
 }
 {
   // 대소문자 변환
+  function solution(s) {
+    let answer = '';
+    for (const x of s) {
+      // (1)
+      // x === x.toUpperCase() ? (answer += x.toLowerCase()) : (answer += x.toUpperCase());
+
+      // (2)
+      if (x === x.toUpperCase()) answer += x.toLowerCase();
+      else answer += x.toUpperCase();
+    }
+
+    return answer;
+  }
+
+  console.log(solution('StuDY'));
 }
 {
   // 가장 긴 문자열
+  function solution(s) {
+    let answer;
+    let max = Number.MIN_SAFE_INTEGER;
+    for (const word of s) {
+      if (word.length > max) {
+        max = word.length;
+        answer = word;
+      }
+    }
+    return answer;
+  }
+
+  function solution2(s) {
+    let answer = '';
+    for (const word of s) {
+      if (word.length > answer.length) answer = word;
+    }
+    return answer;
+  }
+
+  let str = ['teacher', 'time', 'student', 'beautiful', 'good'];
+  console.log(solution(str));
+  console.log(solution2(str));
 }
 {
   // 가운데 문자 출력
+  function solution(s) {
+    let answer;
+    let mid = Math.floor(s.length / 2); // study -> 2
+    // (1) substring()
+    // string 객체의 시작 인덱스로 부터 종료 인덱스 전 까지 문자열의 부분 문자열을 반환
+    if (s.length % 2 === 1) answer = s.substring(mid, mid + 1);
+    else answer = s.substring(mid - 1, mid + 1);
+
+    // (2) substr()
+    // 문자열에서 특정 위치에서 시작하여 특정 문자 수 만큼의 문자들을 반환
+    // if (s.length % 2 === 1) answer = s.substr(mid, 1);
+    // else answer = s.substr(mid - 1, 2);
+
+    return answer;
+  }
+  console.log(solution('study'));
 }
 {
   // 중복문자제거(indexOf)
+  // String 객체에서 주어진 값과 일치하는 첫 번째 인덱스를 반환, 일치하는 값이 없으면 -1을 반환
+  function solution(s) {
+    // (1)
+    let answer = '';
+    for (let i = 0; i < s.length; i++) {
+      // console.log(s[i], i, s.indexOf(s[i]));
+      if (s.indexOf(s[i]) === i) answer += s[i];
+    }
+
+    return answer;
+  }
+  console.log(solution('ksekkset'));
 }
 {
   // 중복단어제거
+  function solution(s) {
+    let answer;
+    // console.log(s.indexOf('time')); // 1
+    answer = s.filter((v, i) => s.indexOf(v) === i);
+
+    return answer;
+  }
+
+  let str = ['good', 'time', 'good', 'time', 'student'];
+  console.log(solution(str));
+}
+{
+  // 중복단어제거(2)
+  function solution(s) {
+    // Array.from
+    // 유사 배열 객체(array-like object)나 반복 가능한 객체(iterable object)를 얕게 복사해 새로운Array 객체를 만든다.
+    let answer;
+    answer = Array.from(new Set(str));
+
+    return answer;
+  }
+
+  let str = ['good', 'time', 'good', 'time', 'student', 'teacher', 'time'];
+  console.log(solution(str));
 }
